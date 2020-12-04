ee2eaa205cd4007615d77c5c77b8ee7df640f9fa
Glandos <bugs-framagit@antipoul.fr>
2020-12-04 23:09:03 +0100

    convent indentation

--- magnetico_merge.py
+++ magnetico_merge.py
@@ -9,24 +9,24 @@ import click
 """
 Schema:
 CREATE TABLE torrents (
-			id             INTEGER PRIMARY KEY,
-			info_hash      BLOB NOT NULL UNIQUE,
-			name           TEXT NOT NULL,
-			total_size     INTEGER NOT NULL CHECK(total_size > 0),
-			discovered_on  INTEGER NOT NULL CHECK(discovered_on > 0)
-		,
-		 updated_on INTEGER CHECK (updated_on > 0) DEFAULT NULL,
-		  n_seeders  INTEGER CHECK ((updated_on IS NOT NULL AND n_seeders >= 0) OR (updated_on IS NULL AND n_seeders IS NULL)) DEFAULT NULL,
-		   n_leechers INTEGER CHECK ((updated_on IS NOT NULL AND n_leechers >= 0) OR (updated_on IS NULL AND n_leechers IS NULL)) DEFAULT NULL,
-		    modified_on INTEGER NOT NULL
-				CHECK (modified_on >= discovered_on AND (updated_on IS NOT NULL OR modified_on >= updated_on))
-				DEFAULT 32503680000);
+            id             INTEGER PRIMARY KEY,
+            info_hash      BLOB NOT NULL UNIQUE,
+            name           TEXT NOT NULL,
+            total_size     INTEGER NOT NULL CHECK(total_size > 0),
+            discovered_on  INTEGER NOT NULL CHECK(discovered_on > 0)
+        ,
+         updated_on INTEGER CHECK (updated_on > 0) DEFAULT NULL,
+          n_seeders  INTEGER CHECK ((updated_on IS NOT NULL AND n_seeders >= 0) OR (updated_on IS NULL AND n_seeders IS NULL)) DEFAULT NULL,
+           n_leechers INTEGER CHECK ((updated_on IS NOT NULL AND n_leechers >= 0) OR (updated_on IS NULL AND n_leechers IS NULL)) DEFAULT NULL,
+            modified_on INTEGER NOT NULL
+                CHECK (modified_on >= discovered_on AND (updated_on IS NOT NULL OR modified_on >= updated_on))
+                DEFAULT 32503680000);
 CREATE TABLE files (
-			id          INTEGER PRIMARY KEY,
-			torrent_id  INTEGER REFERENCES torrents ON DELETE CASCADE ON UPDATE RESTRICT,
-			size        INTEGER NOT NULL,
-			path        TEXT NOT NULL
-		, is_readme INTEGER CHECK (is_readme IS NULL OR is_readme=1) DEFAULT NULL, content   TEXT    CHECK ((content IS NULL AND is_readme IS NULL) OR (content IS NOT NULL AND is_readme=1)) DEFAULT NULL);
+            id          INTEGER PRIMARY KEY,
+            torrent_id  INTEGER REFERENCES torrents ON DELETE CASCADE ON UPDATE RESTRICT,
+            size        INTEGER NOT NULL,
+            path        TEXT NOT NULL
+        , is_readme INTEGER CHECK (is_readme IS NULL OR is_readme=1) DEFAULT NULL, content   TEXT    CHECK ((content IS NULL AND is_readme IS NULL) OR (content IS NOT NULL AND is_readme=1)) DEFAULT NULL);
 CREATE UNIQUE INDEX info_hash_index ON torrents	(info_hash);
 CREATE UNIQUE INDEX readme_index ON files (torrent_id, is_readme);
 CREATE VIRTUAL TABLE torrents_idx USING fts5(name, content='torrents', content_rowid='id', tokenize="porter unicode61 separators ' !""#$%&''()*+,-./:;<=>?@[\]^_`{|}~'")
@@ -36,17 +36,17 @@ CREATE TABLE IF NOT EXISTS 'torrents_idx_idx'(segid, term, pgno, PRIMARY KEY(seg
 CREATE TABLE IF NOT EXISTS 'torrents_idx_docsize'(id INTEGER PRIMARY KEY, sz BLOB);
 CREATE TABLE IF NOT EXISTS 'torrents_idx_config'(k PRIMARY KEY, v) WITHOUT ROWID;
 CREATE TRIGGER torrents_idx_ai_t AFTER INSERT ON torrents BEGIN
-			  INSERT INTO torrents_idx(rowid, name) VALUES (new.id, new.name);
-			END;
+              INSERT INTO torrents_idx(rowid, name) VALUES (new.id, new.name);
+            END;
 CREATE TRIGGER torrents_idx_ad_t AFTER DELETE ON torrents BEGIN
-			  INSERT INTO torrents_idx(torrents_idx, rowid, name) VALUES('delete', old.id, old.name);
-			END;
+              INSERT INTO torrents_idx(torrents_idx, rowid, name) VALUES('delete', old.id, old.name);
+            END;
 CREATE TRIGGER torrents_idx_au_t AFTER UPDATE ON torrents BEGIN
-			  INSERT INTO torrents_idx(torrents_idx, rowid, name) VALUES('delete', old.id, old.name);
-			  INSERT INTO torrents_idx(rowid, name) VALUES (new.id, new.name);
-			END;
+              INSERT INTO torrents_idx(torrents_idx, rowid, name) VALUES('delete', old.id, old.name);
+              INSERT INTO torrents_idx(rowid, name) VALUES (new.id, new.name);
+            END;
 CREATE TRIGGER "torrents_modified_on_default_t" AFTER INSERT ON "torrents" BEGIN
-	          UPDATE "torrents" SET "modified_on" = NEW."discovered_on" WHERE "id" = NEW."id" AND NEW."modified_on" = 32503680000;
+              UPDATE "torrents" SET "modified_on" = NEW."discovered_on" WHERE "id" = NEW."id" AND NEW."modified_on" = 32503680000;
             END;
 CREATE INDEX modified_on_index ON torrents (modified_on);
 """
